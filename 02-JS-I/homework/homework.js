f72b31f77170fda1b3b7c33ca8e82f67f3001f1e
BautistaDlucca <dluccabautista@gmail.com>
2022-12-13 01:51:24 -0300

    homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,10 +4,10 @@
 const nuevaString = "hola!!" 
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 30;
+const nuevoNum =30;
 
 // Crea una variable booleana:
-const nuevoBool = false;
+const nuevoBool =false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
